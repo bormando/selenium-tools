10b300e7e6a4591784808018717f6a3e6e639160
Dmitrii Bormotov <squier7@gmail.com>
2023-04-02 02:06:52 +0400

    Remove webdriver manager execution

--- seletools/tests/conftest.py
+++ seletools/tests/conftest.py
@@ -1,7 +1,5 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope="function")
@@ -10,11 +8,8 @@ def driver():
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("window-size=1024,700")
-    dc = DesiredCapabilities.CHROME
-    dc["goog:loggingPrefs"] = {"browser": "ALL"}
-    browser = webdriver.Chrome(
-        ChromeDriverManager().install(), options=options, desired_capabilities=dc
-    )
+    options.set_capability("goog:loggingPrefs", {"browser": "ALL"})
+    browser = webdriver.Chrome(options=options)
     browser.implicitly_wait(10)
     yield browser
     browser.quit()
